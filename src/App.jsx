d172661d141f2d39ae31e8193012d1fa22cbde13
sabinbabu <sabinacharya444@gmail.com>
2025-03-31 15:35:52 +1100

    login home page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import "./App.css";
-import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignUpButton,
+  SignInButton,
+} from "@clerk/clerk-react";
 import Dashboard from "./Dashboard";
+import { Button, Container, Row, Stack } from "react-bootstrap";
 
 function App() {
   return (
     <>
       <SignedOut>
-        <SignInButton />
+        <Container>
+          <Row className="vh-100 ">
+            <Stack className="d-flex justify-content-center align-items-center">
+              <h1>Note</h1>
+              <p>Note Taking made easy and simple</p>
+              <p>Not registered with us? </p>
+
+              <SignUpButton>
+                <Button variant="link">Sign up</Button>
+              </SignUpButton>
+
+              <p>Login to see your notes</p>
+
+              <SignInButton>
+                <Button variant="link">Login</Button>
+              </SignInButton>
+            </Stack>
+          </Row>
+        </Container>
       </SignedOut>
+
       <SignedIn>
         <Dashboard />
       </SignedIn>
