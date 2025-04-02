08a33de1ddd83b1e060c9319a4d24a3ec4dc4d8d
sabinbabu <sabinacharya444@gmail.com>
2025-04-02 15:12:01 +1100

    controlled from added

--- src/components/SingleNoteCard.jsx
+++ src/components/SingleNoteCard.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, Stack } from "react-bootstrap";
+import { Button, Card, Stack, Form } from "react-bootstrap";
 import { format } from "date-fns";
 import { AiOutlineEdit } from "react-icons/ai";
 import { TiTick } from "react-icons/ti";
@@ -7,7 +7,12 @@ import { useState } from "react";
 const SingleNoteCard = ({ noteData }) => {
   const { note, updatedAt = "", backgroundColor } = noteData;
   const [isEditMode, setIsEditMode] = useState(false);
-  console.log(noteData);
+  const [form, setForm] = useState({ ...noteData });
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...note, [name]: value });
+  };
 
   const handleOnEditClick = () => {
     setIsEditMode(true);
@@ -21,7 +26,25 @@ const SingleNoteCard = ({ noteData }) => {
       style={{ width: "18rem", height: "18rem", borderRadius: "2rem" }}
     >
       <Card.Body className="p-4">
-        <Card.Text>{note}</Card.Text>
+        {isEditMode && (
+          <Form>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Control
+                className={`${backgroundColor} border-0`}
+                as="textarea"
+                name="note"
+                value={form.note}
+                rows={6}
+                onChange={handleOnChange}
+                autoFocus
+              />
+            </Form.Group>
+          </Form>
+        )}
+        {!isEditMode && <Card.Text>{note}</Card.Text>}
       </Card.Body>
       <Card.Footer>
         <Stack
